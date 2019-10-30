e9bed71798bae1203f866f0a7d616b84ba90b1d7
Sean <tomcruise81@users.noreply.github.com>
2019-10-30 17:29:09 -0400

    Remove additions to PATH

--- .travis/test.sh
+++ .travis/test.sh
@@ -3,7 +3,6 @@
 if [ "$TRAVIS_OS_NAME" = 'windows' ]; then
     # Re-activate the virtualenv
     source "ve/python${PYTHON_VERSION}/Scripts/activate"
-    export PATH="./bin:${PATH}"
 fi
 
 make test

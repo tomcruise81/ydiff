fc69ead0f495b124095a4cd5aaec817ed97f3515
Sean <tomcruise81@users.noreply.github.com>
2019-10-31 09:41:08 -0400

    Cleaned up empty if

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$TRAVIS_OS_NAME" = 'windows' ]; then
+# if [ "$TRAVIS_OS_NAME" = 'windows' ]; then
     # Re-activate the virtualenv
     # source "envs/python${PYTHON_VERSION}/Scripts/activate"
-fi
+# fi
 
 make test

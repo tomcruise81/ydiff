c84d566eac86427180a8de136b6871db22e7c92e
Sean <tomcruise81@users.noreply.github.com>
2019-10-31 09:35:17 -0400

    Switch from ve to envs; Disable activate

--- .travis/test.sh
+++ .travis/test.sh
@@ -2,7 +2,7 @@
 
 if [ "$TRAVIS_OS_NAME" = 'windows' ]; then
     # Re-activate the virtualenv
-    source "ve/python${PYTHON_VERSION}/Scripts/activate"
+    # source "envs/python${PYTHON_VERSION}/Scripts/activate"
 fi
 
 make test

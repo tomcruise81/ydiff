a32ba4a2d9b71281f1ef68333bbb64e0ced3fba6
Sean <tomcruise81@users.noreply.github.com>
2019-10-31 17:14:27 -0400

    Still working to get git commands working on Windows

--- tests/test_ydiff.py
+++ tests/test_ydiff.py
@@ -794,9 +794,9 @@ class MainTest(unittest.TestCase):
 
         cwd = self._cwd
         os.chdir(self._ws)
-        cmd = ('git init; git config user.name me; '
-               'git config user.email me@example.org')
-        subprocess.call(cmd, shell=True, stdout=subprocess.PIPE)
+        cmd = ['git init; git config user.name me; '
+               'git config user.email me@example.org']
+        subprocess.call(cmd, stdout=subprocess.PIPE)
         self._change_file('init')
         os.chdir(cwd)
 

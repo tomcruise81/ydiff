141378c5b260066567fed946a64469973722a727
Sean <tomcruise81@users.noreply.github.com>
2019-11-01 07:54:02 -0400

    Try os.system instead

--- tests/test_ydiff.py
+++ tests/test_ydiff.py
@@ -794,14 +794,14 @@ class MainTest(unittest.TestCase):
 
         cwd = self._cwd
         os.chdir(self._ws)
-        # os.system('git init && '
-        #           'git config user.name me && '
-        #           'git config user.email me@example.org')
-        subprocess.call(['git', 'init'], stdout=subprocess.PIPE)
-        subprocess.call(['git', 'config', 'user.name', 'me'],
-                        stdout=subprocess.PIPE)
-        subprocess.call(['git', 'config', 'user.email', 'me@example.org'],
-                        stdout=subprocess.PIPE)
+        os.system('git init && '
+                  'git config user.name me && '
+                  'git config user.email me@example.org')
+        # subprocess.call(['git', 'init'], stdout=subprocess.PIPE)
+        # subprocess.call(['git', 'config', 'user.name', 'me'],
+        #                 stdout=subprocess.PIPE)
+        # subprocess.call(['git', 'config', 'user.email', 'me@example.org'],
+        #                 stdout=subprocess.PIPE)
         self._change_file('init')
         os.chdir(cwd)
 
